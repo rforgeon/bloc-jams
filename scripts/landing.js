7ea1fa42f72f9e6c29b6a9984eb3139eb0bb82d0
rforgeon <rforgeon@gmail.com>
2016-08-24 10:29:19 -0700

    Checkpoint 9 events: Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName("point");
 
-       var points = document.getElementsByClassName('point');
-
+var animatePoints = function(points){
        var revealPoint = function(index) {
            points[index].style.opacity = 1;
            points[index].style.transform = "scaleX(1) translateY(0)";
@@ -14,3 +13,17 @@ var animatePoints = function() {
        }
 
     };
+
+window.onload = function() {
+  var sellingPoints = document.getElementsByClassName("selling-points")[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener('scroll', function(event) {
+    console.log("Current offset from the top is "+ sellingPoints.getBoundingClientRect().top+ " pixels");
+    if (document.documentElement.scollTop || document.body.scrollTop >= scrollDistance){
+
+        animatePoints(pointsArray);
+    }
+
+     });
+}
